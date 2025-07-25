a520a398523757b8145e4e283cda75e3826d6772
SkhumbuzoT <109624354+SkhumbuzoT@users.noreply.github.com>
2025-07-25 07:48:12 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Hello, Vercel!</h1>
+      <p>This is a placeholder homepage. Work in progress...</p>
+    </div>
+  );
+}
